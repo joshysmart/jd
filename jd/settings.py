85c7dd2d77178be1de23822922840a1e4aaaa8c3
Joshysmart <joshysmart@Joshysmarts-MacBook-Pro.local>
2023-02-05 14:44:34 +0100

    first commit

--- jd/settings.py
+++ jd/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-AUTH_TOKEN = config('AUTH_TOKEN')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

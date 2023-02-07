58895f510add2e26cd8726e1f297df33e1d7738f
Joshysmart <joshysmart@Joshysmarts-MacBook-Pro.local>
2023-02-07 09:09:33 +0100

    fix symptom searching

--- symptoms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from .models import Symptom
-
-
-# class SymptomAdmin(admin.ModelAdmin):
-#     list_display = ("name", "id")
-
-
-# admin.site.register(Symptom, SymptomAdmin)
